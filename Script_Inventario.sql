429871dc943f8adad7ed8e9c699a310054aad62a
Alejandro Ramos Celaya <46717209+Aramoscelaya@users.noreply.github.com>
2025-02-04 12:01:00 -0500

    Add files via upload

---
+++ Script_Inventario.sql
@@ -0,0 +1,97 @@
+CREATE SCHEMA `Inventario` ;
+
+CREATE TABLE `inventario`.`categorias` (
+	`id_categoria` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre_categoria` VARCHAR(100) NOT NULL UNIQUE,
+    `descripcion_categoria` TEXT,
+    `estatus` TINYINT(1) NOT NULL DEFAULT 1,
+    `fecha_creacion` TIMESTAMP NOT NULL)
+COMMENT = 'Tabla para clasificar el producto o equipo';
+
+CREATE TABLE `inventario`.`areas` (
+	`id_area` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`nombre_area` VARCHAR(45) NOT NULL,
+	`estatus` TINYINT(1) NOT NULL,
+	`fecha_creacion` TIMESTAMP NOT NULL)
+COMMENT = 'Tabla para identificar las campañas (areas) que contienen equipos de computo';
+
+CREATE TABLE `inventario`.`productos` (
+	`id_producto` VARCHAR(250) NOT NULL PRIMARY KEY,
+    `nombre_producto` VARCHAR(150) NOT NULL,
+    `modelo` VARCHAR(150) NOT NULL,
+    `marca` VARCHAR(150) NOT NULL,
+    `hostname` VARCHAR(50) NULL,
+    `descripcion_producto` TEXT,
+    `stock` INT NOT NULL DEFAULT 1,
+    `estatus`TINYINT(1) NOT NULL DEFAULT 1,
+    `id_area` INT,    
+    `id_categoria` INT,
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`fecha_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`usuario_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`id_area`) REFERENCES areas(`id_area`) ON DELETE SET NULL,
+	FOREIGN KEY (`id_categoria`) REFERENCES categorias(`id_categoria`) ON DELETE SET NULL)
+COMMENT = 'Tabla para almacenar productos pc - lap - ups';
+
+CREATE TABLE `inventario`.`usuarios` (
+    `id_usuario` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`nombre` VARCHAR(150) NOT NULL,
+	`usuario` VARCHAR(150) NOT NULL,
+    `password` VARCHAR(255) NULL,
+    `rol` ENUM('admin', 'colaborador') NOT NULL DEFAULT 'colaborador',
+    `estatus`TINYINT(1) NOT NULL DEFAULT 1,
+    `id_area` INT,
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`fecha_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`usuario_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`id_area`) REFERENCES areas(`id_area`) ON DELETE SET NULL)
+COMMENT = 'Tabla para usuarios del sisitema TI - agentes A - Supervisor S';
+
+CREATE TABLE `inventario`.`mantenimientos` (
+    `id_mantenimiento` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `estado_manto` INT NOT NULL DEFAULT 1,
+    `descripcion_manto` TEXT,
+    `estatus`TINYINT(1) NOT NULL DEFAULT 1,
+    `id_producto` VARCHAR(250),
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`fecha_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`usuario_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`id_producto`) REFERENCES productos(`id_producto`) ON DELETE SET NULL)
+COMMENT = 'estado_manto / Pendiente 1 - Diagnóstico 2 - En curso 3 - Manto preventivo 4 - Manto correctivo 5 - Listo 6';
+
+CREATE TABLE `inventario`.`asignaciones` (
+    `id_asinacion` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `estatus`TINYINT(1) NOT NULL DEFAULT 1,
+    `id_usuario` INT,
+	`id_producto` VARCHAR(250),
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`fecha_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`usuario_modificacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`id_usuario`) REFERENCES usuarios(`id_usuario`) ON DELETE SET NULL,
+    FOREIGN KEY (`id_producto`) REFERENCES productos(`id_producto`) ON DELETE SET NULL)
+COMMENT = 'Tabla para tener registro de las asignaciones Estatus / entregado 1 - devuelto 2 - perdido 3';
+
+CREATE TABLE `inventario`.`logs` (
+	`id_log` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `vista` VARCHAR(100) NOT NULL,
+    `categoria` VARCHAR(100) NOT NULL,
+    `descripcion_log` TEXT,
+    `fecha_creacion` TIMESTAMP NOT NULL)
+COMMENT = 'Tabla para clasificar los logs';
+
+/*
+id_producto-PF4HD0K,
+nombre_producto-Laptop,
+modelo-E14,
+marca-Lenovo,
+hostname-WPHI002
+https://es.qr-code-generator.com
+*/
+
+
+
+
+
+
+
+
